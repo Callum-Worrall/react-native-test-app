577266c3fd8e2b0d1b54490dfcc0491529f584e9
Callum-Worrall <callum.worrall71@gmail.com>
2022-02-20 18:08:58 +1100

    Adding MoodCheckScreen which was also hooked up to Context, but was missed in the commit..

--- app/screens/MoodCheckScreen.js
+++ app/screens/MoodCheckScreen.js
@@ -8,59 +8,38 @@ import {
 } from '@react-native-community/hooks';
 
 import colors from '../config/colors';
-
+import MoodGrid from '../components/MoodGrid'
 import MoodButton from '../components/MoodButton'
 
 export default function MoodCheckScreen({navigation}) {
 
   console.log("Mood Check In Screen");
   const {landscape} = useDeviceOrientation();
-
-  const moodStringArray = [
-    'Lonely', 'Happy', 'Confused',
-    'Bored', 'Anxious', 'Disappointed',
-    'Angry', 'Sad', 'Optimistic'
-  ]
-
-  const createMoodButtons = () => {
-    console.log("Create Mood Buttons")//, moodArray)
-    return moodStringArray.map((mood, index) => (
-      // <MoodButton />
-      <MoodButton key={index} id={index} name={mood} selected={false}/>
-    ))
-  }
-
-  const createGrid = () => {
-    const buttonArray = createMoodButtons();
-    return(
-      <View style={contentStyles.buttonGrid}>
-        {createRow(buttonArray, 0)}
-        {createRow(buttonArray, 1)}
-        {createRow(buttonArray, 2)}
-      </View>
-    )
-  }
-
-  const createRow = (buttonArray, rowNum) => {    
-    console.log("Row: ", rowNum, " created.")
-    return(
-    <View style={contentStyles.row}>
-      {buttonArray[rowNum + 0]}
-      {buttonArray[rowNum + 1]}
-      {buttonArray[rowNum + 2]}
-    </View>
-    )
-  }
-
+  
   return (
     <SafeAreaView style={pageStyles.screen}>
       <View style={[pageStyles.navbar, pageStyles.topNavbar]}><Text>Top Navbar</Text></View>
       <View style={pageStyles.content}>
         <View style={contentStyles.selectionPrompt}>
-          <Text>How are you feeling?</Text>
+          <Text
+            numberOfLines={1}// add this 
+            adjustsFontSizeToFit// add this
+            style={{ fontSize: 28, bottom: 20 }}
+          >
+            How are you feeling?
+          </Text>
         </View>
-        {createGrid()}
-        <View style={contentStyles.continue}><Text>Continue</Text></View>
+        <MoodGrid style={contentStyles.moodGrid}/>        
+        <View style={contentStyles.continue}>
+        <TouchableOpacity onPress={() =>
+          navigation.navigate('IndexScreen')}><Text
+            numberOfLines={1}// add this 
+            adjustsFontSizeToFit// add this
+            style={{ fontSize: 28, bottom: 10, right: 10 }}//, bottom: 10, left: 10 }}
+          >
+            ->
+          </Text></TouchableOpacity>
+        </View>
       </View>      
       <View style={[pageStyles.navbar, pageStyles.bottomNavbar]}><Text>Bottom Navbar</Text></View>
     </SafeAreaView>
@@ -72,19 +51,18 @@ const pageStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center', //vertical
     alignItems: 'center', //horizontal
-    backgroundColor: colors.primary,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, //status bar for android as SafeAreaView only works iOS
   },
 
   navbar: {
     flex: 1,
     width: 350,
-    backgroundColor: "green",
+    // backgroundColor: "green",
   },
 
   content: {
     flex: 10,
-    backgroundColor: "blue",
+    // backgroundColor: "blue",
     justifyContent: "center",
     alignItems: "center"
   },
@@ -102,36 +80,19 @@ const pageStyles = StyleSheet.create({
   },
 });
 
-const contentStyles = StyleSheet.create({
-  
+const contentStyles = StyleSheet.create({  
   selectionPrompt: {
     flex: 1.5,
-    fontSize: 24,
     width: 350,
-    justifyContent: 'center',
-    backgroundColor: "red",
+    justifyContent: "flex-end",
+    // backgroundColor: "red",
   },
 
-  buttonGrid: {
-    flex: 5,
-    backgroundColor: "dodgerblue",
-    width: 350,
-    height: 330,
-    justifyContent: "space-evenly",
-    alignItems: "stretch"
-  },
-
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-  },
-    
   continue: {
     flex: 1.5,
     width: 350,
-    backgroundColor: "red",
+    // backgroundColor: "red",
     flexDirection: "row-reverse",
-    alignItems: "center",
-    // alignItems: "flex-end"
+    alignItems: "flex-end"
   },
 });
